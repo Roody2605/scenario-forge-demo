43afcaf00479ee251d49f9e2423008a686006c78
Roody2605 <roody.elkady@gmail.com>
2025-05-31 16:32:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,2 @@
-import streamlit as st
-import pandas as pd
-from io import StringIO
+st.sidebar.success("✅ App loaded")     # DEBUG banner
+st.write("👋 Hello — the app is alive") # Force something on screen
